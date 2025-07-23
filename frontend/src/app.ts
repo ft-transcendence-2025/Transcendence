6776e8839c4a21bf373ce1372d80a71c6c972a47
AijaRe <aija.repsa@gmail.com>
2025-07-23 21:56:33 +0100

    start the jungle

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -7,23 +7,23 @@ const contentElement = document.getElementById("content");
 
 // listen for browser back / forward navigation
 window.addEventListener("popstate", () => {
-    router(contentElement);
+  router(contentElement);
 });
 
 // listen for dom to be fully loaded
 document.addEventListener("DOMContentLoaded", async () => {
-    await renderNavbar(navbarElement); // render the navbar component
-    // global click listener for navigation links (data-links)
-    document.body.addEventListener("click", (event) => {
-        const target = event.target as HTMLElement;
-        if (target.matches("[data-link]")) {
-            event.preventDefault(); // prevent default link behavior (full page reload)
-            const path = target.getAttribute("href");
-            if (path) {
-                navigateTo(path, contentElement);
-            }
-        }
-    });
+  await renderNavbar(navbarElement); // render the navbar component
+  // global click listener for navigation links (data-links)
+  document.body.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    if (target.matches("[data-link]")) {
+      event.preventDefault(); // prevent default link behavior (full page reload)
+      const path = target.getAttribute("href");
+      if (path) {
+        navigateTo(path, contentElement);
+      }
+    }
+  });
 
-    router(contentElement); // load home view on initial run
+  router(contentElement); // load home view on initial run
 });
