89119af82af9dfb45fc78bca769922dbf14e6e62
Uatilla Viana Almeida <uatilaalmeida@proton.me>
2025-07-26 17:56:42 +0100

    Working contract

--- 2_Avalanche/scripts/deploy.js
+++ 2_Avalanche/scripts/deploy.js
@@ -1,11 +1,18 @@
 const hre = require("hardhat"); //package Hardhat R Environment
 
 async function main() {
+    const counterTest = await hre.ethers.deployContract("CounterTest");
+    console.log("Deploying counterTest...");
+    await counterTest.waitForDeployment();
+    console.log(`counterTest deployed to: ${counterTest.target}`);
+}
+
+/*async function main() {
     const simpleStorage = await hre.ethers.deployContract("SimpleStorage");
     console.log("Deploying SimpleStorage...");
     await simpleStorage.waitForDeployment();
     console.log(`SimpleStorage deployed to: ${simpleStorage.target}`);
-}
+}*/
 
 main().catch((error) => {
     console.error(error);
