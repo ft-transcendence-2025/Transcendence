e32460408d390fd478ca12abbf192762830a45e4
jhonas <jhonas.burlamaqui@gmail.com>
2025-07-30 08:39:52 +0100

    added ssl certificate

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
-const fastify = require('fastify')()
+const fs = require('fs');
 const fastifyStatic = require('@fastify/static')
 const path = require('node:path')
+const fastify = require('fastify')({
+  https: {
+    key: fs.readFileSync(path.join(__dirname, '../ssl/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '../ssl/cert.pem'))
+  }
+})
 
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, '../dist'),
