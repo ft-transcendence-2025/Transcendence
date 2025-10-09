f7d8fb51eaaeb9e01f48c012f8e824a0ec97b0c5
ebmarque <ebenezermarquezine@gmail.com>
2025-10-09 09:22:25 +0100

    refactor: update blockchain proxy to use authentication preHandler

--- src/routes/blockchain.proxy.ts
+++ src/routes/blockchain.proxy.ts
@@ -3,14 +3,15 @@ import { FastifyPluginAsync, FastifyInstance } from "fastify";
 import proxy from "@fastify/http-proxy";
 import { blockchainPreHandler } from "../middlewares/blockchain-prehandler";
 
-const blockchainProxy: FastifyPluginAsync = async (app: FastifyInstance) => {
+const blockchainProxy: FastifyPluginAsync = async (app: any) => {
    
   //Catch all proxy
   app.register(proxy, {
     upstream: "http://blockchain:3000",
     prefix: "/api/blockchain",
     rewritePrefix: "/",
-    preHandler: blockchainPreHandler(app),
+    preHandler: [app.authenticate],
+    // preHandler: blockchainPreHandler(app),
   });
 
 };
